5b4115dcc3e2ba385aec977cede76d7c64d5b5b6
pahaz <pahaz.blinov@gmail.com>
2016-06-26 15:31:14 +0500

    ordering example

--- sources/todo/views.py
+++ sources/todo/views.py
@@ -10,7 +10,7 @@ def home(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-    items = Item.objects.all()
+    items = Item.objects.all().order_by('-id')
     counter = redis.incr('counter')
     return render(request, 'home.html', {'items': items, 'counter': counter})
 
