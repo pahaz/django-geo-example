179a59e199c7da8a0e5cad1aa95a3194cdbd68f3
pahaz <pahaz.blinov@gmail.com>
2016-06-26 11:58:19 +0500

    add task delay argument support

--- sources/todo/tasks.py
+++ sources/todo/tasks.py
@@ -1,13 +1,17 @@
 from __future__ import absolute_import
 
+from time import sleep
+
 from celery import shared_task
 
 from . import redis
 
 
 @shared_task
-def increase(count):
+def increase(count, delay=1):
     assert count > 0, 'count > 0'
+    assert delay >= 0, 'delay >= 0'
     for _ in range(count):
         counter = redis.incr('counter')
+        sleep(delay)
     return counter
