f6eb11ce779eb4df6c053b3ff173788b99514caf
pahaz <pahaz.blinov@gmail.com>
2017-06-12 00:41:05 +0500

    geoapi.views: add base ViewSets

--- sources/geoapi/views.py
+++ sources/geoapi/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render, redirect
+from rest_framework import viewsets
+from rest_framework_gis.pagination import GeoJsonPagination
+
+from .models import ServiceArea, Provider
+from .serializers import ServiceAreaSerializer, ProviderSerializer
 
 
-def home(request):
-    if request.method == 'POST':
-        return redirect('/')
-    return render(request, 'home.html', {})
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    serializer_class = ServiceAreaSerializer
+    queryset = ServiceArea.objects.all()
+    pagination_class = GeoJsonPagination
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    serializer_class = ProviderSerializer
+    queryset = Provider.objects.all()
