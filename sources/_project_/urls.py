cab470dec7bcb45b6963964b5bbda25d60bf65f9
pahaz <pahaz.blinov@gmail.com>
2017-06-12 00:42:27 +0500

    _project_.urls: add index view

--- sources/_project_/urls.py
+++ sources/_project_/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib.gis import admin
+from django.shortcuts import redirect, render
 
 __author__ = 'pahaz'
 
 
+def index(request):
+    if request.method == 'POST':
+        return redirect('/')
+    return render(request, 'index.html', {})
+
+
 urlpatterns = [
+    url(r'^$', index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('geoapi.urls')),
 ]
